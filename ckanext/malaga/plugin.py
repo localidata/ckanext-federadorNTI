55872456f8370c0f87d0577117a330b2f9c2b08d
Juan Carlos Ballesteros <jcballesteros@localidata.com>
2018-03-06 13:59:19 +0100

    Directorios actualizados

--- ckanext/malaga/plugin.py
+++ ckanext/malaga/plugin.py
@@ -152,8 +152,8 @@ class malagae(p.SingletonPlugin):
 
     def update_config(self, config):
         # add template directory that contains our snippet
-		p.toolkit.add_template_directory(config, '/usr/lib/ckan/default/src/ckanext-malaga/ckanext/malaga/theme/templates')
-		p.toolkit.add_public_directory (config, '/usr/lib/ckan/default/src/ckanext-malaga/ckanext/malaga/public')
+		p.toolkit.add_template_directory(config, '/usr/lib/ckan/default/src/ckanext-federadorNTI/ckanext/malaga/theme/templates')
+		p.toolkit.add_public_directory (config, '/usr/lib/ckan/default/src/ckanext-federadorNTI/ckanext/malaga/public')
 
     #iruiz: register helper function
     def get_helpers(self):
